20cb5e1a8511d468f57658616e115c5f6a1af4f8
Carl Andrei Lambino <veriasdan@gmail.com>
2022-05-31 20:26:22 +0800

    commit 3

--- TicTacToe/Form1.cs
+++ TicTacToe/Form1.cs
@@ -18,68 +18,122 @@ namespace TicTacToe
 
         private void box1_Click(object sender, EventArgs e)
         {
-
+            if (box1.Text == "")
+            {
+                box1.Text = "X";
+            }
+            else
+            {
+                box1.Text = "O";    
+            }
         }
 
         private void box2_Click(object sender, EventArgs e)
         {
-
+            if (box2.Text == "")
+            {
+                box2.Text = "X";
+            }
+            else
+            {
+                box2.Text = "O";
+            }
         }
 
         private void box3_Click(object sender, EventArgs e)
         {
-
+            if (box3.Text == "")
+            {
+                box3.Text = "X";
+            }
+            else
+            {
+                box3.Text = "O";
+            }
         }
 
         private void box4_Click(object sender, EventArgs e)
         {
-
+            if (box4.Text == "")
+            {
+                box4.Text = "X";
+            }
+            else
+            {
+                box4.Text = "O";
+            }
         }
 
         private void box5_Click(object sender, EventArgs e)
         {
-
+            if (box5.Text == "")
+            {
+                box5.Text = "X";
+            }
+            else
+            {
+                box5.Text = "O";
+            }
         }
 
         private void box6_Click(object sender, EventArgs e)
         {
-
+            if (box6.Text == "")
+            {
+                box6.Text = "X";
+            }
+            else
+            {
+                box6.Text = "O";
+            }
         }
 
         private void box7_Click(object sender, EventArgs e)
         {
-
+            if (box7.Text == "")
+            {
+                box7.Text = "X";
+            }
+            else
+            {
+                box7.Text = "O";
+            }
         }
 
         private void box8_Click(object sender, EventArgs e)
         {
-
+            if (box8.Text == "")
+            {
+                box8.Text = "X";
+            }
+            else
+            {
+                box8.Text = "O";
+            }
         }
 
         private void box9_Click(object sender, EventArgs e)
         {
-
+            if (box9.Text == "")
+            {
+                box9.Text = "X";
+            }
+            else
+            {
+                box9.Text = "O";
+            }
         }
         private void newGameToolStripMenuItem_Click(object sender, EventArgs e)
         {
             box1.Text = "";
-            box1.BackColor = Color.White;
             box2.Text = "";
-            box2.BackColor = Color.White;
             box3.Text = "";
-            box3.BackColor = Color.White;
             box4.Text = "";
-            box4.BackColor = Color.White;
             box5.Text = "";
-            box5.BackColor = Color.White;
             box6.Text = "";
-            box6.BackColor = Color.White;
             box7.Text = "";
-            box7.BackColor = Color.White;
             box8.Text = "";
-            box8.BackColor = Color.White;
             box9.Text = "";
-            box9.BackColor = Color.White;
 
         }
     }
