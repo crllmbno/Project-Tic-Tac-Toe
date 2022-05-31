59e6a85c5d65b6eae037f4565d89560c30450d6b
Carl Andrei Lambino <veriasdan@gmail.com>
2022-05-31 21:13:04 +0800

    experiment worked

--- TicTacToe/Form1.cs
+++ TicTacToe/Form1.cs
@@ -2,6 +2,8 @@ namespace TicTacToe
 {
     public partial class TicTacWindow : Form
     {
+        bool order = true;
+        int counter = 0;
         public TicTacWindow()
         {
             InitializeComponent();
@@ -9,7 +11,7 @@ namespace TicTacToe
 
         private void helpToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hello");
+            MessageBox.Show("Firt to complete a row wins!");
         }
         private void exitToolStripMenuItem_Click(object sender, EventArgs e)
         {
@@ -18,132 +20,114 @@ namespace TicTacToe
 
         private void box1_Click(object sender, EventArgs e)
         {
-            if (box1.Text == "")
-            {
+            if (order)
                 box1.Text = "X";
-            }
             else
-            {
                 box1.Text = "O";
-                box1.Enabled = false;  
-            }
+
+            order = !order;
+            box1.Enabled = false;
             checkwin();
         }
 
         private void box2_Click(object sender, EventArgs e)
         {
-            if (box2.Text == "")
-            {
+            if (order)
                 box2.Text = "X";
-            }
             else
-            {
                 box2.Text = "O";
-                box2.Enabled = false;
-            }
+
+            order = !order;
+            box2.Enabled = false;
             checkwin();
         }
 
         private void box3_Click(object sender, EventArgs e)
         {
-            if (box3.Text == "")
-            {
+            if (order)
                 box3.Text = "X";
-            }
             else
-            {
                 box3.Text = "O";
-                box3.Enabled = false;
-            }
+
+            order = !order;
+            box3.Enabled = false;
             checkwin();
         }
 
         private void box4_Click(object sender, EventArgs e)
         {
-            if (box4.Text == "")
-            {
+            if (order)
                 box4.Text = "X";
-            }
             else
-            {
                 box4.Text = "O";
-                box4.Enabled = false;
-            }
+
+            order = !order;
+            box4.Enabled = false;
             checkwin();
         }
 
         private void box5_Click(object sender, EventArgs e)
         {
-            if (box5.Text == "")
-            {
+            if (order)
                 box5.Text = "X";
-            }
             else
-            {
                 box5.Text = "O";
-                box5.Enabled = false;
-            }
+
+            order = !order;
+            box5.Enabled = false;
             checkwin();
         }
 
         private void box6_Click(object sender, EventArgs e)
         {
-            if (box6.Text == "")
-            {
+            if (order)
                 box6.Text = "X";
-            }
             else
-            {
                 box6.Text = "O";
-                box6.Enabled = false;
-            }
+
+            order = !order;
+            box6.Enabled = false;
             checkwin();
         }
 
         private void box7_Click(object sender, EventArgs e)
         {
-            if (box7.Text == "")
-            {
+            if (order)
                 box7.Text = "X";
-            }
             else
-            {
                 box7.Text = "O";
-                box7.Enabled = false;
-            }
+
+            order = !order;
+            box7.Enabled = false;
             checkwin();
         }
 
         private void box8_Click(object sender, EventArgs e)
         {
-            if (box8.Text == "")
-            {
+            if (order)
                 box8.Text = "X";
-            }
             else
-            {
                 box8.Text = "O";
-                box8.Enabled = false;
-            }
+
+            order = !order;
+            box8.Enabled = false;
             checkwin();
         }
 
         private void box9_Click(object sender, EventArgs e)
         {
-            if (box9.Text == "")
-            {
+            if (order)
                 box9.Text = "X";
-            }
             else
-            {
                 box9.Text = "O";
-                box9.Enabled = false;
-            }
+
+            order = !order;
+            box9.Enabled = false;
             checkwin();
         }
         private void checkwin()
         {
-            if ((box1.Text == box2.Text) && (box2.Text == box3.Text) && (!box1.Enabled)) 
+            if ((box1.Text == box2.Text) && (box2.Text == box3.Text) && (!box1.Enabled))
                 MessageBox.Show("yeet");
             else if ((box4.Text == box5.Text) && (box5.Text == box6.Text) && (!box4.Enabled))
                 MessageBox.Show("yeet");
@@ -157,14 +141,23 @@ namespace TicTacToe
         private void newGameToolStripMenuItem_Click(object sender, EventArgs e)
         {
             box1.Text = "";
+            box1.Enabled = true;
             box2.Text = "";
+            box2.Enabled = true;
             box3.Text = "";
+            box3.Enabled = true;
             box4.Text = "";
+            box4.Enabled = true;
             box5.Text = "";
+            box5.Enabled = true;
             box6.Text = "";
+            box6.Enabled = true;
             box7.Text = "";
+            box7.Enabled = true;
             box8.Text = "";
+            box8.Enabled = true;
             box9.Text = "";
+            box9.Enabled = true;
         }
     }
 }
